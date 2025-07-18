617e3f936afc5014b072348e4046446f4f8a46d6
kalkidan <kalkidanteshale6@gmail.com>
2025-07-18 19:54:59 +0300

    create a project using the create-next-app cli-tool

--- alx-project-0x00/pages/_app.tsx
+++ alx-project-0x00/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "<tsconfig. json>/styles/globals.css";
+import "<tsconfig.json>/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
